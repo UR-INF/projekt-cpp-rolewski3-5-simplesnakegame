c6a53135b4898916f0ab28aaef8112e36d0a4886
unknown <damianrolewski@gmail.com>
2019-12-02 19:39:30 +0100

    utworzone okno graficzne i dodanie biblioteki

---
+++ snake.cpp
@@ -0,0 +1,14 @@
+#include<graphics.h>
+#include<time.h>
+#include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+
+int main(){
+    int gd,gm;
+    detectgraph(&gd,&gm);
+    initgraph(&gd,&gm,"");
+    delay(2000);
+
+}
+
